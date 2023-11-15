9d3acc32ab437a921ece8217ba79558bf5e649fb
wucunqi <wucunqi163@163.com>
2023-11-15 16:02:03 +0800

    初始化

---
+++ src/main/java/com/wucunqi/mall/storage/MallStorageApplication.java
@@ -0,0 +1,13 @@
+package com.wucunqi.mall.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wucunqi/mall/storage/MallStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wucunqi.mall.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
